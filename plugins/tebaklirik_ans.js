bb7e53f323b2fc057bd1f9ab8053f5f49c7dab06
Kanna-Chan <77791484+irham01@users.noreply.github.com>
2022-08-06 12:31:14 +0700

    Update tebaklirik_ans.js

--- plugins/tebaklirik_ans.js
+++ plugins/tebaklirik_ans.js
@@ -10,9 +10,7 @@ handler.before = async function (m) {
         let json = JSON.parse(JSON.stringify(this.tebaklirik[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebaklirik[id][2]
-            global.db.data.users[m.sender].tiketcoin += 1
-            m.reply(`*Benar!*\n+${this.tebaklirik[id][2]} XP\n+1 Tiketcoin`)
+            m.reply(`*Benar!*`)
             clearTimeout(this.tebaklirik[id][3])
             delete this.tebaklirik[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
