1bec183c4e455264c7ba8991fa234dec8164ffb1
Kanna-Chan <77791484+irham01@users.noreply.github.com>
2022-08-06 12:22:24 +0700

    Update suitpvp.js

--- plugins/suitpvp.js
+++ plugins/suitpvp.js
@@ -1,6 +1,4 @@
 let timeout = 60000
-let poin = 500
-let poin_lose = -100
 let handler = async (m, { conn, usedPrefix }) => {
   conn.suit = conn.suit ? conn.suit : {}
   if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Selesaikan suit mu yang sebelumnya'
@@ -24,7 +22,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]}
     waktu: setTimeout(() => {
       if (conn.suit[id]) conn.reply(m.chat, `_Waktu suit habis_`, m)
       delete conn.suit[id]
-    }, timeout), poin, poin_lose, timeout
+    }, timeout), timeout
   }
 }
 handler.tags = ['game']
