c77ec1e5985b3931dbefd2216873f7441be4aa57
Kanna-Chan <77791484+irham01@users.noreply.github.com>
2022-08-06 12:29:22 +0700

    Update tebakkata_ans.js

--- plugins/tebakkata_ans.js
+++ plugins/tebakkata_ans.js
@@ -10,9 +10,7 @@ handler.before = async function (m) {
         let json = JSON.parse(JSON.stringify(this.tebakkata[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebakkata[id][2]
-            global.db.data.users[m.sender].tiketcoin += 1
-            m.reply(`*Benar!*\n+${this.tebakkata[id][2]} XP\n+1 Tiketcoin`)
+            m.reply(`*Benar!*`)
             clearTimeout(this.tebakkata[id][3])
             delete this.tebakkata[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
