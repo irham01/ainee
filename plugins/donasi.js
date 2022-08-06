3da346b6a980a0b55346eb1014824ef681873d78
Kanna-Chan <77791484+irham01@users.noreply.github.com>
2022-08-06 12:07:05 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • 」
+│ •  [0856-0151-7617 == DANA]
+│ •  [0856-0151-7617 == SPAY]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
